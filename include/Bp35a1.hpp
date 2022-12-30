e9ddbf79b19d641b86cc56b1a2b3246718b1e0c4
aki <ak1211@mail.ak1211.com>
2022-12-31 01:02:43 +0900

    touch

--- include/Bp35a1.hpp
+++ include/Bp35a1.hpp
@@ -406,8 +406,8 @@ std::optional<Response> receive_response(Stream &commport) {
       if (x.length() > 0) {
         // まだ何か残っていたら後ろに追加する
         std::copy(x.begin(), x.end(), std::back_inserter(ev.data));
-        std::reverse(ev.data.begin(), ev.data.end());
         // データの長さになるように前を削る(おそらく空白が入り込んでいる)
+        std::reverse(ev.data.begin(), ev.data.end());
         ev.data.resize(ev.datalen.u16);
         std::reverse(ev.data.begin(), ev.data.end());
       }
